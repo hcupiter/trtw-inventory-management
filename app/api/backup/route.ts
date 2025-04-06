f31d0e89e06d5f58077ed4e2453526940a8a7a24
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2025-04-06 14:55:43 +0700

    feat: implemented backup database

--- app/api/backup/route.ts
+++ app/api/backup/route.ts
@@ -1,4 +1,3 @@
-import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import fs from "fs";
@@ -10,22 +9,15 @@ export async function POST(req: Request) {
   try {
     const cookie = await cookies();
     const sessionToken = cookie.get("session")?.value;
-    const lastBackup = cookie.get("trtwLastBackup")?.value;
 
     if (!sessionToken) return NextResponse.json({ error: "Invalid Credentials" }, { status: 500 });
-
-    if (lastBackup)
-      return NextResponse.json(
-        { message: "Last backup is less than a week, no need to backup" },
-        { status: 200 }
-      );
 
     // Define the path to your SQLite database (adjust if needed)
     const dbPath = path.join(process.cwd(), "database.sqlite");
 
     // Check if the database file exists
     if (!fs.existsSync(dbPath))
-      return NextResponse.json({ error: "Database is not found!" }, { status: 404 });
+      return NextResponse.json({ error: "Database tidak ditemukan" }, { status: 404 });
 
     // Read the database file into a Buffer
     const fileBuffer = fs.readFileSync(dbPath);
@@ -52,27 +44,9 @@ export async function POST(req: Request) {
     if (response.status < 200 || response.status >= 300)
       return NextResponse.json({ error: "Failed to backup" }, { status: 500 });
 
-    setBackupCookie(cookie); // Set backup cookie if successfull
-
-    return NextResponse.json(
-      { message: `successfully backup the database with id ${response.result.id}` },
-      { status: 200 }
-    );
+    return NextResponse.json({ message: `Backup sukses` }, { status: 200 });
   } catch (error) {
     console.log("Error at api/backup: ", error);
     return NextResponse.json({ error: errorWriter(error) }, { status: 500 });
   }
 }
-
-const setBackupCookie = (cookie: ReadonlyRequestCookies) => {
-  // Set the cookie securely
-  cookie.set({
-    name: "trtwLastBackup",
-    value: new Date().toDateString(),
-    httpOnly: true, // Prevents JavaScript access (more secure)
-    secure: process.env.NODE_ENV === "production", // Secure in production
-    path: "/", // Available on all routes
-    maxAge: 60 * 60 * 24 * 7, // 7 day expiration
-    sameSite: "lax", // Helps prevent CSRF attacks
-  });
-};
